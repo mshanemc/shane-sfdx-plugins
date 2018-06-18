481391124a3da1d0b848ac71d0cd2f413eeb0482
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-06-18 09:48:37 -0500

    actually implement github partial src

--- src/commands/shane/github/src/install.ts
+++ src/commands/shane/github/src/install.ts
@@ -1,14 +1,9 @@
 import { flags } from '@oclif/command';
-import { join } from 'path';
 import { SfdxCommand, core } from '@salesforce/command';
-import fs = require('fs-extra');
 import util = require('util');
-import request = require('request-promise-native');
 import child_process = require('child_process');
 
 const exec = util.promisify(child_process.exec);
-
-const tmpDir = 'githubMdapiTmp';
 
 export default class GithubPackageInstall extends SfdxCommand {
 
@@ -28,35 +23,29 @@ export default class GithubPackageInstall extends SfdxCommand {
   protected static flagsConfig = {
     githubUser: flags.string({ required: true, char: 'g', description: 'github username where the package lives' }),
     repo: flags.string({ required: true, char: 'r', description: 'repo where the packages lives' }),
-    path: flags.string({ default: 'src', char: 'r', description: 'folder where the source lives' })
+    path: flags.string({ default: 'src', char: 'p', description: 'folder where the source lives' }),
+    keepLocally: flags.boolean({char: 'k', description: 'keep the cloned repo in local source instead of deleting it'})
     // branch: flags.string({ char: 'b', description: 'optional branch' })
   };
 
   protected static requiresProject = true;
 
   public async run(): Promise<any> { // tslint:disable-line:no-any
-    fs.ensureDirSync(tmpDir);
 
-    // download the source and put into the folder
-    // may have to be a clone operation
-    // // clone to tempCloneDir, single branch
-    // // cp the path folder to the tmpDir
-    // // rm the cloneDir
-    const url = `https://github.com/${this.flags.githubUser}/${this.flags.repo}/src`;
-    // if (this.flags.branch) {
-    //   this.ux.error(chalk.red('branch handling is not implemented yet'));
-    //   return false;
-    // }
+    const repoUrl = `https://github.com/${this.flags.githubUser}/${this.flags.repo}`;
 
-    // const result = await request.get({
-    //   url,
-    //   json: true
-    // });
+    const gitResult = await exec(`git clone ${repoUrl}`);
+    this.ux.log(gitResult.stderr);
 
     // install in the org
-    const installResult = await exec(`sfdx force:mdapi:deploy -d ${tmpDir} -u ${this.org.getUsername()} -w 20 --json`);
-    console.log(installResult);
-    return installResult;
+    const installResult = await exec(`sfdx force:mdapi:deploy -d ${this.flags.repo}/${this.flags.path} -u ${this.org.getUsername()} -w 20 --json`);
+    this.ux.logJson(JSON.parse(installResult.stdout));
+
+    if (!this.flags.keepLocally) {
+      await exec(`rm -rf ${this.flags.repo}`);
+    }
+
+    return JSON.parse(installResult.stdout);
   }
 
 }
