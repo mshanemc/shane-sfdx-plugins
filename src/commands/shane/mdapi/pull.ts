c738e77b3d7d49ba58174ded3c917b3c493848d2
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-07-02 16:00:05 -0500

    rework pull command to dynamically build xmls like all the other commands

--- src/commands/shane/mdapi/pull.ts
+++ src/commands/shane/mdapi/pull.ts
@@ -3,12 +3,15 @@ import { SfdxCommand, core } from '@salesforce/command';
 import fs = require('fs-extra');
 import util = require('util');
 
-// source code for packages
-import code = require('../../../shared/packageXmls/code_xml');
-import all = require('../../../shared/packageXmls/all_xml');
-import perms = require('../../../shared/packageXmls/perms_xml');
-import schema = require('../../../shared/packageXmls/schema_xml');
-import ui = require('../../../shared/packageXmls/ui_xml');
+import jsToXml = require('js2xmlparser');
+import * as options from '../../../shared/js2xmlStandardOptions';
+
+const code = ['ApexClass', 'ApexTrigger', 'ApexComponent', 'ApexPage', 'AuraDefinitionBundle', 'StaticResource'];
+const perms = ['PermissionSet', 'Profile', 'Role', 'CustomPermission', 'Group'];
+const schema = ['ExternalDataSource', 'CustomMetadata', 'RecordType', 'GlobalValueSet', 'CustomField', 'CustomObject'];
+const standardObjects = ['Account', 'AccountContactRelation', 'AccountContactRole', 'Activity', 'Asset', 'AssistantProgress', 'Campaign', 'CampaignMember', 'Case', 'CaseContactRole', 'Contact', 'ContentVersion', 'Contract', 'ContractContactRole', 'DuplicateRecordItem', 'DuplicateRecordSet', 'EmailMessage', 'Event', 'ExchangeUserMapping', 'FeedItem', 'Idea', 'Lead', 'LinkedArticle', 'Macro', 'MacroAction', 'MacroInstruction', 'Opportunity', 'OpportunityCompetitor', 'OpportunityContactRole', 'OpportunityLineItem', 'Order', 'OrderItem', 'PartnerRole', 'Pricebook2', 'PricebookEntry', 'Product2', 'ProfileSkill', 'ProfileSkillEndorsement', 'ProfileSkillUser', 'Quote', 'QuoteLineItem', 'Site', 'SocialPersona', 'Solution', 'StreamingChannel', 'Task', 'Territory', 'User', 'WorkBadge', 'WorkBadgeDefinition', 'WorkThanks'];
+const ui = ['CompactLayout', 'Layout', 'ListView', 'CustomTab', 'AppMenu', 'CustomApplication', 'CustomPageWebLink', 'HomePageComponent', 'HomePageLayout', 'PathAssistant', 'WebLink', 'CustomLabels'];
+const all = ['AccountCriteriaBasedSharingRule', 'AccountOwnerSharingRule', 'AnalyticSnapshot', 'ApexClass', 'ApexComponent', 'ApexPage', 'ApexTrigger', 'ApprovalProcess', 'AppMenu', 'AssignmentRules', 'AuraDefinitionBundle', 'AuthProvider', 'AutoResponseRules', 'Certificate', 'CleanDataService', 'Community', 'CompactLayout', 'CustomApplication', 'CustomApplicationComponent', 'CustomField', 'CustomLabels', 'CustomObject', 'CustomMetadata', 'CustomObjectTranslation', 'CustomPageWebLink', 'CustomPermission', 'CustomSite', 'CustomTab', 'DelegateGroup', 'DuplicateRule', 'EscalationRules', 'ExternalDataSource', 'Flow', 'FlowDefinition', 'GlobalValueSet', 'GlobalValueSetTranslation', 'Group', 'HomePageComponent', 'HomePageLayout', 'Layout', 'Letterhead', 'ListView', 'ManagedTopics', 'MatchingRule', 'MatchingRules', 'Network', 'PathAssistant', 'PermissionSet', 'Profile', 'Queue', 'QuickAction', 'RecordType', 'RemoteSiteSetting', 'ReportType', 'Role', 'SharingRules', 'SharingCriteriaRule', 'SharingOwnerRule', 'SharingTerritoryRule', 'SiteDotCom', 'StandardValueSetTranslation', 'StaticResource', 'Territory', 'Translations', 'ValidationRule', 'WebLink', 'Workflow', 'WorkflowAlert', 'WorkflowFieldUpdate', 'WorkflowRule', 'Settings'];
 
 import child_process = require('child_process');
 
@@ -24,6 +27,9 @@ export default class Pull extends SfdxCommand {
   public static examples = [
 `sfdx shane:mdapi:pull -c -u someOrg
 // pulls code kinda stuff from the org and converts/merges it into force-app
+`,
+`sfdx shane:mdapi:pull -t ExternalDataSource -u someOrg
+// pulls all the external data source metadata from the org and converts/merges it into force-app
 `
   ];
 
@@ -35,8 +41,9 @@ export default class Pull extends SfdxCommand {
     schema: flags.boolean({ char: 's', description: 'Pull objects, fields, list views, recordtypes, valueSets, custom Metadata' }),
     ui: flags.boolean({ char: 'i', description: 'Pull page layouts, tabs, compact layouts, apps, tabs, more' }),
     object: flags.string({ char: 'o', description: 'pull metadata for a single object'}),
+    type: flags.string({char: 't', description: 'pull only a specific type.  See the metadata api docs for type names'}),
     // TODO: automation, security, reporting, i18n
-    all: flags.boolean({description: 'Pulls just about everything.  Don\'t use this flag with any other subset of metadata.  Not recommended for really large metatadat orgs because it\'ll overflow stdout' }),
+    all: flags.boolean({ description: 'Pulls just about everything.  Don\'t use this flag with any other subset of metadata.  Not recommended for really large metatadat orgs because it\'ll overflow stdout'}),
     target: flags.string({ char: 't', default: 'force-app', description: 'where to convert the result to...defaults to force-app' })
   };
 
@@ -52,51 +59,104 @@ export default class Pull extends SfdxCommand {
       this.ux.error('you can\'t choose a single object AND schema');
     }
 
+    if (this.flags.type && this.flags.code && code.includes(this.flags.type)) {
+      this.ux.error(`type ${this.flags.type} is already included in --code`);
+    }
+
+    if (this.flags.type && this.flags.ui && ui.includes(this.flags.type)) {
+      this.ux.error(`type ${this.flags.type} is already included in --ui`);
+    }
+
+    if (this.flags.type && this.flags.schema && schema.includes(this.flags.type)) {
+      this.ux.error(`type ${this.flags.type} is already included in --schema`);
+    }
+
+    if (this.flags.type && this.flags.perms && perms.includes(this.flags.type)) {
+      this.ux.error(`type ${this.flags.type} is already included in --perms`);
+    }
+
+    const packageJSON = {
+      '@': {
+        xmlns: 'http://soap.sforce.com/2006/04/metadata'
+      },
+      'types': [],
+      'version': this.org.retrieveMaxApiVersion()
+    };
+
     if (this.flags.all) {
-      fs.writeFileSync(`./${pkgDir}/package.xml`, all.xml);
-      await this.pullUnzipConvert('all');
+      all.forEach((item) => {
+        if (item === 'CustomObject') {
+          packageJSON.types.push({
+            members: standardObjects.concat(['*']),
+            name: item
+          });
+        } else {
+          packageJSON.types.push({
+            members: '*',
+            name: item
+          });
+        }
+      });
     } else {
-      // it's individual subsets of metadata
+
+      // things that can be used in combination
       if (this.flags.object) {
-        const singleObject = `<?xml version="1.0" encoding="UTF-8"?>
-<Package xmlns="http://soap.sforce.com/2006/04/metadata">
-	<types>
-		<members>${this.flags.object}</members>
-		<name>CustomObject</name>
-	</types>
-	<version>41.0</version>
-</Package>`;
-
-        fs.writeFileSync(`./${pkgDir}/package.xml`, singleObject);
-        await this.pullUnzipConvert(this.flags.object);
+        packageJSON.types.push({
+          members: this.flags.object,
+          name: 'CustomObject'
+        });
       }
 
-      if (this.flags.code) {
-        fs.writeFileSync(`./${pkgDir}/package.xml`, code.xml);
-        await this.pullUnzipConvert('code');
-      }
-
-      if (this.flags.perms) {
-        fs.writeFileSync(`./${pkgDir}/package.xml`, perms.xml);
-        await this.pullUnzipConvert('perms');
+      if (this.flags.type) {
+        packageJSON.types.push({
+          members: '*',
+          name: this.flags.type
+        });
       }
 
       if (this.flags.schema) {
-        fs.writeFileSync(`./${pkgDir}/package.xml`, schema.xml);
-        await this.pullUnzipConvert('schema');
+        schema.forEach( (item) => {
+          packageJSON.types.push({
+            members: '*',
+            name: item
+          });
+        });
       }
 
       if (this.flags.ui) {
-        fs.writeFileSync(`./${pkgDir}/package.xml`, ui.xml);
-        await this.pullUnzipConvert('ui');
+        ui.forEach((item) => {
+          packageJSON.types.push({
+            members: '*',
+            name: item
+          });
+        });
       }
 
+      if (this.flags.perms) {
+        perms.forEach((item) => {
+          packageJSON.types.push({
+            members: '*',
+            name: item
+          });
+        });
+      }
+
+      if (this.flags.code) {
+        code.forEach((item) => {
+          packageJSON.types.push({
+            members: '*',
+            name: item
+          });
+        });
+      }
     }
 
-  }
+    // this.ux.logJson(packageJSON);
 
-  private async pullUnzipConvert(thing: string) {
-    process.stdout.write(`Starting retrieval for ${thing}...`);
+    const xml = jsToXml.parse('Package', packageJSON, options.js2xmlStandardOptions);
+    fs.writeFileSync(`./${pkgDir}/package.xml`, xml);
+
+    process.stdout.write('Starting retrieval...');
 
     const retrieveResult = await exec(`sfdx force:mdapi:retrieve -s -k ${pkgDir}/package.xml -r ./${tmpDir} -w 30 -u ${this.org.getUsername()}`, { maxBuffer: 1000000 * 1024 });
 
@@ -113,13 +173,14 @@ export default class Pull extends SfdxCommand {
 
     try {
       const convertResult = await exec(`sfdx force:mdapi:convert -r ./${tmpDir} -d ${this.flags.target} --json`);
-      process.stdout.write('done.  Cleaning up...');
+      process.stdout.write(`done (converted ${JSON.parse(convertResult.stdout).result.length} items).  Cleaning up...`);
     } catch (err) {
       this.ux.errorJson(err);
-      this.ux.error('Converting failed--it may have been too much metadata');
+      this.ux.error('Error from conversion--it may have been too much metadata');
     }
 
     await exec(`rm -rf ./${tmpDir}`);
     process.stdout.write('Done!\n');
+
   }
 }
