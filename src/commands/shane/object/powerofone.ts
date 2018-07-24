731c16183161aface7b7204753accd3cad7ceaf2
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-07-24 12:02:50 -0500

    power of one

---
+++ src/commands/shane/object/powerofone.ts
@@ -0,0 +1,72 @@
+import { SfdxCommand } from '@salesforce/command';
+import fs = require('fs-extra');
+import jsToXml = require('js2xmlparser');
+
+import * as options from '../../../shared/js2xmlStandardOptions';
+
+import chalk from 'chalk';
+
+export default class PowerOfOne extends SfdxCommand {
+
+  public static description = 'create a remote site setting in the local source.  Push it when you\'re done';
+
+  public static examples = [
+`sfdx shane:object:powerofone -a Poo -l "Power of One" -o User
+// create a field with api name Poo__c and label "Power of One" on the user object with the default description in the default folder
+`
+  ];
+
+  protected static flagsConfig = {
+    object: { type: 'string', char: 'o', required: true, description: 'API name of the object to add the field to' },
+    label: { type: 'string', char: 'l', default: 'Power Of One', description: 'label' },
+    api: { type: 'string', char: 'a', default: 'Power_Of_One__c', description: 'api name (will include the __c for you if you don\'t add it here' },
+    description: { type: 'string', char: 'd', default: 'Power of one is used for formulas, reporting, etc', description: 'optional description so you can remember why you added this and what it\'s for' },
+    target: { type: 'string', char: 't', default: 'force-app/main/default', description: 'where to create the folder (if it doesn\'t exist already) and file...defaults to force-app/main/default' }
+  };
+
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  protected static requiresProject = true;
+
+  public async run(): Promise<any> { // tslint:disable-line:no-any
+
+    if (this.flags.api.includes(' ')) {
+      this.ux.error(chalk.red('spaces are not allowed in the api name'));
+    }
+
+    if (!this.flags.api.includes('__c')) {
+      this.flags.api = `${this.flags.api}__c`;
+    }
+
+    // remove trailing slash if someone entered it
+    if (this.flags.target.endsWith('/')) {
+      this.flags.target = this.flags.target.substring(0, this.flags.target.length - 1);
+    }
+
+    fs.ensureDirSync(`${this.flags.target}/objects`);
+    fs.ensureDirSync(`${this.flags.target}/objects/${this.flags.object}`);
+    fs.ensureDirSync(`${this.flags.target}/objects/${this.flags.object}/fields`);
+
+    const fieldJSON = {
+      '@': {
+        xmlns: 'http://soap.sforce.com/2006/04/metadata'
+      },
+      fullName: this.flags.api,
+      externalId: false,
+      formula: 1,
+      formulaTreatBlanksAs: 'BlankAsZero',
+      label: this.flags.label,
+      precision: 18,
+      required: false,
+      scale: 0,
+      type: 'Number',
+      unique: false,
+      description: this.flags.description
+    };
+
+    const xml = jsToXml.parse('CustomField', fieldJSON, options.js2xmlStandardOptions);
+
+    fs.writeFileSync(`${this.flags.target}/objects/${this.flags.object}/fields/${this.flags.api}.field-meta.xml`, xml);
+
+    this.ux.log(`${chalk.green('Power of One field created for you')}.  It's only local...push to deploy.`);
+  }
+}
---
+++ test/commands/shane/object/poo.test.ts
@@ -0,0 +1,120 @@
+/* tslint:disable:no-unused-expression */
+
+import { expect, test } from '@salesforce/command/dist/test';
+import fs = require('fs-extra');
+import util = require('util');
+import xml2js = require('xml2js');
+
+import child_process = require('child_process');
+
+import testutils = require('../../../helpers/testutils');
+
+const exec = util.promisify(child_process.exec);
+
+const testProjectName = 'testProject';
+const api = 'Corgi__c';
+const label = 'Corgi';
+const plural = 'Corgi';
+
+describe('shane:object:create (regular object flavor)', () => {
+
+  before(async () => {
+    await exec(`rm -rf ${testProjectName}`);
+    await exec(`sfdx force:project:create -n ${testProjectName}`);
+  });
+
+  it('creates an object with all params supplied for a Text Name', async () => {
+
+    // `sfdx shane:object:create --label "Platypus" --plural "${plural}" --api Platypus__b --directory /my / project / path
+
+    await exec(`sfdx shane:object:create --type custom --label "${label}" --plural "${plural}" --api ${api} --nametype Text --sharingmodel ReadWrite`, { cwd: testProjectName });
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/${api}`)).to.be.true;
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/${api}/fields`)).to.be.true;
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/${api}/${api}.object-meta.xml`)).to.be.true;
+
+    const parsed = await testutils.getParsedXML(`${testProjectName}/force-app/main/default/objects/${api}/${api}.object-meta.xml`);
+
+    expect(parsed.CustomObject).to.be.an('object');
+    expect(parsed.CustomObject.deploymentStatus).to.equal('Deployed');
+    expect(parsed.CustomObject.label).to.equal(label);
+    expect(parsed.CustomObject.pluralLabel).to.equal(plural);
+    expect(parsed.CustomObject.eventType).to.be.undefined;
+    expect(parsed.CustomObject.sharingModel).to.equal('ReadWrite');
+  });
+
+  it('adds poo on custom object', async () => {
+
+    const apiname = 'Power_Of_One__c';
+    const fieldLabel = 'Power Of One';
+
+    await exec(`sfdx shane:object:powerofone -o ${api}`, { cwd: testProjectName });
+
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/${api}`)).to.be.true;
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/${api}/fields`)).to.be.true;
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/${api}/fields/${apiname}.field-meta.xml`)).to.be.true;
+
+    const parsed = await testutils.getParsedXML(`${testProjectName}/force-app/main/default/objects/${api}/fields/${apiname}.field-meta.xml`);
+
+    expect(parsed.CustomField).to.be.an('object');
+    expect(parsed.CustomField.unique).to.equal('false');
+    expect(parsed.CustomField.type).to.equal('Number');
+    expect(parsed.CustomField.precision).to.equal('18');
+    expect(parsed.CustomField.fullName).to.equal(apiname);
+    expect(parsed.CustomField.label).to.equal(fieldLabel);
+  });
+
+  it('adds poo on standard object that is not local', async () => {
+
+    const apiname = 'Power_Of_One__c';
+    const fieldLabel = 'Power Of One';
+
+    await exec('sfdx shane:object:powerofone -o Account', { cwd: testProjectName });
+
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/Account`)).to.be.true;
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/Account/fields`)).to.be.true;
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/Account/fields/${apiname}.field-meta.xml`)).to.be.true;
+
+    const parsed = await testutils.getParsedXML(`${testProjectName}/force-app/main/default/objects/Account/fields/${apiname}.field-meta.xml`);
+
+    expect(parsed.CustomField).to.be.an('object');
+    expect(parsed.CustomField.unique).to.equal('false');
+    expect(parsed.CustomField.type).to.equal('Number');
+    expect(parsed.CustomField.precision).to.equal('18');
+    expect(parsed.CustomField.fullName).to.equal(apiname);
+    expect(parsed.CustomField.label).to.equal(fieldLabel);
+  });
+
+  it('adds poo on standard object with custom name/label and apply __c', async () => {
+
+    const apiname = 'Poo';
+    const fieldLabel = 'The Poo field';
+
+    await exec(`sfdx shane:object:powerofone -o Case -l "${fieldLabel}" -a "${apiname}"`, { cwd: testProjectName });
+
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/Case`)).to.be.true;
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/Case/fields`)).to.be.true;
+    expect(fs.existsSync(`${testProjectName}/force-app/main/default/objects/Case/fields/${apiname}__c.field-meta.xml`)).to.be.true;
+
+    const parsed = await testutils.getParsedXML(`${testProjectName}/force-app/main/default/objects/Case/fields/${apiname}__c.field-meta.xml`);
+
+    expect(parsed.CustomField).to.be.an('object');
+    expect(parsed.CustomField.unique).to.equal('false');
+    expect(parsed.CustomField.type).to.equal('Number');
+    expect(parsed.CustomField.precision).to.equal('18');
+    expect(parsed.CustomField.fullName).to.equal(`${apiname}__c`);
+    expect(parsed.CustomField.label).to.equal(fieldLabel);
+  });
+
+  it('deploys as valid code', async () => {
+    if (process.env.LOCALONLY === 'true') {
+      console.log('skipping online-only test');
+    } else {
+      const deploySuccess = await testutils.itDeploys(testProjectName);
+      expect(deploySuccess).to.be.true;
+    }
+  });
+
+  after(async () => {
+    await exec(`rm -rf ${testProjectName}`);
+  });
+});
