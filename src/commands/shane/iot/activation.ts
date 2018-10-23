201f1a1b67c9a4909fa1dd887613906f621fd603
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-10-23 11:06:44 -0500

    activate orchestration context before orchestration

--- src/commands/shane/iot/activation.ts
+++ src/commands/shane/iot/activation.ts
@@ -26,7 +26,7 @@ export default class Activation extends SfdxCommand {
   protected static requiresProject = false;
 
   public async run(): Promise<any> { // tslint:disable-line:no-any
-
+    const version = '44.0';
     const conn = this.org.getConnection();
     const auth = {
       bearer: conn.accessToken
@@ -37,7 +37,7 @@ export default class Activation extends SfdxCommand {
     };
 
     // first, get the orchestration Id
-    const orchestrations = JSON.parse(await request.get(`${conn.instanceUrl}/services/data/v42.0/iot/orchestrations`, {
+    const orchestrations = JSON.parse(await request.get(`${conn.instanceUrl}/services/data/v${version}/iot/orchestrations`, {
       auth
     })).orchestrations;
     // console.log(orchestrations);
@@ -53,8 +53,29 @@ export default class Activation extends SfdxCommand {
       this.ux.error(chalk.red(`No orchestration found matching that name.  Orchestrations found: ${orchNames.join(', ')}`));
       return;
     } else {
-      this.ux.log(`found orchestration with id ${thisOrch.id}.  Activating...`);
+      // this.ux.logJson(thisOrch);
+      this.ux.log(`found orchestration with id ${thisOrch.id}.  Will activate its context ${thisOrch.orchestrationContext.label}`);
     }
+
+    const contextActivationResult = await request.post({
+      url: `${conn.instanceUrl}/services/data/v${version}/iot/contexts/${thisOrch.orchestrationContext.id}/activations`,
+      body: {},
+      auth,
+      json: true
+    });
+    // this.ux.logJson(contextActivationResult);
+
+    // wait for a sign that the context is active
+    let contextActive = false;
+    do {
+      const context = JSON.parse(await request.get(`${conn.instanceUrl}/services/data/v${version}/iot/contexts/${thisOrch.orchestrationContext.id}`, {
+        auth
+      }));
+      // this.ux.logJson(context);
+      if (context.activationStatus === 'Active') {
+        contextActive = true;
+      }
+    } while (!contextActive);
 
     if (this.flags.deactivate) {
       const deactivationResult = await request.patch({
@@ -66,7 +87,7 @@ export default class Activation extends SfdxCommand {
         auth,
         json: true
       });
-      this.ux.log(chalk.green('Orchestration deactivated.'));
+      this.ux.log(chalk.green('Orchestration deactivating.'));
       return deactivationResult;
     } else {
       const activationResult = await request.post({
@@ -78,7 +99,22 @@ export default class Activation extends SfdxCommand {
         json: true
       });
       this.ux.log(chalk.green('Orchestration activating.'));
+      // wait for a sign that the context is active
+      let orchActive = false;
+      do {
+        const newOrch = JSON.parse(await request.get(`${conn.instanceUrl}/services/data/v${version}/iot/orchestrations/${thisOrch.id}`, {
+          auth
+        }));
+        // this.ux.logJson(newOrch);
+        if (newOrch.status === 'Active') {
+          orchActive = true;
+          this.ux.log(chalk.green('Orchestration is active.'));
+          return newOrch;
+        }
+      } while (!orchActive);
+
       return activationResult;
     }
   }
+
 }
