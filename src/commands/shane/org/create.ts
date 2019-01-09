2cf655ad56cc71d3de87ee4a5ee4014d30658542
mshanemc <shane.mclaughlin@salesforce.com>
2019-01-09 10:05:29 -0600

    BREAK: remove -u option (user full userprefix) and -v for hub targetting

--- src/commands/shane/org/create.ts
+++ src/commands/shane/org/create.ts
@@ -1,43 +1,86 @@
-import { SfdxCommand } from '@salesforce/command';
+import { flags, SfdxCommand } from '@salesforce/command';
+import chalk from 'chalk';
 import * as child_process from 'child_process';
 import request = require('request-promise-native');
 import util = require('util');
 
 const exec = util.promisify(child_process.exec);
-import chalk from 'chalk';
 
 const usernameURL = 'https://unique-username-generator.herokuapp.com/unique';
 
-export default class Delete extends SfdxCommand {
-
-  public static description = 'create an org with a friendly username.  wraps force:org:create';
+export default class CreateOrg extends SfdxCommand {
+  public static description =
+    'create an org with a friendly username.  wraps force:org:create';
 
   public static examples = [
-`sfdx shane:org:create -u shane -o org.test
+    `sfdx shane:org:create --userprefix shane -o org.test
 // creates an org from the default project config/project-scratch-def.json but with username shane[i]@org.test where i is a unique sequence number for that -u/-o combination
 `,
-`sfdx shane:org:create -u shane -o org.test -a sydneyBristow -d 30 -v myOtherHub -f config/thatOtherFile.json
+    `sfdx shane:org:create --userprefix shane -o org.test -a sydneyBristow -d 30 -v myOtherHub -f config/thatOtherFile.json
 // above, but with an alias, a longer duration, and not the default hub, and not the default config file
 `
   ];
 
   protected static flagsConfig = {
-    userprefix: { type: 'string',  char: 'u', required: true, description: 'first part of the generated username.  Example: \'shane\' produces shane1@demo.org, shane2@demo.org' },
-    userdomain: { type: 'string',  char: 'o', required: true, description: 'last part of the generated username (after the @ sign).  Example: \'demo.org\' produces shane1@demo.org, shane2@demo.org' },
-    clientid: { type: 'string',  char: 'i', description: 'connected app consumer key' },
-    definitionfile: { type: 'string',  char: 'f', default: 'config/project-scratch-def.json', description: 'path to a scratch org definition file.  Default = config/project-scratch-def.json' },
-    setalias: { type: 'string',  char: 'a', description: 'set an alias for for the created scratch org' },
-    durationdays: { type: 'number',  char: 'd', default: '7', description: 'duration of the scratch org (in days) (default:7, min:1, max:30)' },
-    wait: { type: 'number', description: 'the streaming client socket timeout (in minutes) (default:20, min:2)' },
-    noancestors: { type: 'boolean',  char: 'c', description: 'do not include second-generation package ancestors in the scratch org' },
-    nonamespace: { type: 'boolean',  char: 'n', description: 'creates the scratch org with no namespace' },
-    setdefaultusername: { type: 'boolean',  char: 's', description: 'set the created org as the default username' },
-    targetdevhubusername: { type: 'boolean',  char: 'v', description: 'username or alias for the dev hub org; overrides default dev hub org' }
+    userprefix: flags.string({
+      required: true,
+      description:
+        'first part of the generated username.  Example: \'shane\' produces shane1@demo.org, shane2@demo.org'
+    }),
+    userdomain: flags.string({
+      char: 'o',
+      required: true,
+      description:
+        'last part of the generated username (after the @ sign).  Example: \'demo.org\' produces shane1@demo.org, shane2@demo.org'
+    }),
+    clientid: flags.string({
+      char: 'i',
+      description: 'connected app consumer key'
+    }),
+    definitionfile: flags.filepath({
+      char: 'f',
+      default: 'config/project-scratch-def.json',
+      description:
+        'path to a scratch org definition file.  Default = config/project-scratch-def.json'
+    }),
+    setalias: flags.string({
+      char: 'a',
+      description: 'set an alias for for the created scratch org'
+    }),
+    durationdays: flags.integer({
+      char: 'd',
+      default: 7,
+      min: 1,
+      max: 30,
+      description:
+        'duration of the scratch org (in days) (default:7, min:1, max:30)'
+    }),
+    wait: flags.integer({
+      description:
+        'the streaming client socket timeout (in minutes) (default:20, min:2)',
+      default: 20,
+      min: 2
+    }),
+    noancestors: flags.boolean({
+      char: 'c',
+      description:
+        'do not include second-generation package ancestors in the scratch org'
+    }),
+    nonamespace: flags.boolean({
+      char: 'n',
+      description: 'creates the scratch org with no namespace'
+    }),
+    setdefaultusername: flags.boolean({
+      char: 's',
+      description: 'set the created org as the default username'
+    })
+    // targetdevhubusername: flags.boolean({description: 'username or alias for the dev hub org; overrides default dev hub org' })
   };
 
   protected static requiresProject = true;
 
-  public async run(): Promise<any> { // tslint:disable-line:no-any
+  public async run(): Promise<any> {
+    // tslint:disable-line:no-any
     // gets the unique username
     const usernameResult = await request.post({
       uri: usernameURL,
@@ -49,7 +92,9 @@ export default class Delete extends SfdxCommand {
     });
 
     // required flags
-    let command = `sfdx force:org:create --json -f ${this.flags.definitionfile} -d ${this.flags.durationdays} -w ${this.flags.wait || 20}`;
+    let command = `sfdx force:org:create --json -f ${
+      this.flags.definitionfile
+    } -d ${this.flags.durationdays} -w ${this.flags.wait || 20}`;
 
     // optional value 	flags without defaults
     if (this.flags.clientid) {
@@ -86,7 +131,13 @@ export default class Delete extends SfdxCommand {
     if (response.stdout) {
       const success = JSON.parse(response.stdout);
       if (success.status === 0) {
-        this.ux.log(chalk.green(`Org created with id ${success.result.orgId} and username ${success.result.username} `));
+        this.ux.log(
+          chalk.green(
+            `Org created with id ${success.result.orgId} and username ${
+              success.result.username
+            } `
+          )
+        );
       } else {
         this.ux.log(chalk.red(response.stderr));
       }
@@ -94,6 +145,5 @@ export default class Delete extends SfdxCommand {
     } else {
       return JSON.parse(response.stdout);
     }
-
   }
 }
