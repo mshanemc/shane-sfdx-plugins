ef8f72e090cb4671f04407473c52fb712b2b2a09
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-05 15:40:51 -0500

    activate actually works

--- src/commands/shane/communities/activate.ts
+++ src/commands/shane/communities/activate.ts
@@ -53,6 +53,30 @@ export default class CommunityActivate extends SfdxCommand {
 
         await newtab.waitForSelector('a.js-workspace-administration');
         await newtab.click('a.js-workspace-administration');
+
+        // it's in another stupid iframe!!
+        const activateIframeTitle = `Settings ~ ${this.flags.name}`;
+        const activateIframeTitleSelector = `iframe[title="${activateIframeTitle}"]`;
+
+        newtab.on('dialog', async dialog => {
+          await newtab.waitFor(1500);
+          await dialog.accept();
+          await newtab.waitFor(1500);
+          await browser.close();
+          this.ux.stopSpinner('Activated community');
+          return true;
+        });
+
+        await newtab.waitForSelector(activateIframeTitleSelector);
+        for (const innerFrame of newtab.frames()) {
+          const innerTitle = await innerFrame.title();
+          if (innerTitle === activateIframeTitle) {
+            await innerFrame.waitForSelector('input[value="Activate Community"]');
+            await innerFrame.click('input[value="Activate Community"]');
+
+          }
+        }
+
         // <a class="js-workspace-administration slds-text-link--reset slds-m-bottom--small slds-text-align--center slds-box--border js-workspace-tile communitySetupCmcWorkspaceTile" href="javascript:void(0);" data-aura-rendered-by="58:119;a" data-aura-class="communitySetupCmcWorkspaceTile"><div class="slds-m-top--large" data-aura-rendered-by="59:119;a"><lightning-icon class="slds-shrink-none administration slds-icon-standard-custom slds-icon_container" data-data-rendering-service-uid="131" data-aura-rendered-by="61:119;a"><lightning-primitive-icon lightning-primitiveicon_primitiveicon-host=""><svg lightning-primitiveIcon_primitiveIcon="" focusable="false" data-key="custom" aria-hidden="true" class="slds-icon slds-icon_large"><use lightning-primitiveIcon_primitiveIcon="" xlink:href="/externalid/_slds/icons/standard-sprite/svg/symbols.svg?cache=9.24.0#custom"></use></svg></lightning-primitive-icon></lightning-icon></div><div class="slds-text-heading--small slds-m-top--small js-tile-label" data-aura-rendered-by="62:119;a"><strong data-aura-rendered-by="63:119;a">Administration</strong></div><div class="slds-text-body--regular slds-text-color--weak slds-m-top--small slds-m-horizontal--small js-tile-description" data-aura-rendered-by="65:119;a">Configure settings and properties for your community.</div></a>
         // // verify that it actually changed value from the click?
         // await frame.waitForSelector('input.enableWaveInCommunities:checked');
@@ -62,7 +86,6 @@ export default class CommunityActivate extends SfdxCommand {
         // // this.ux.log('clicked save');
         // await frame.waitFor(500);
         // await browser.close();
-        // // this.ux.stopSpinner('Activated analytics for communities');
         // return true;
       }
     }
