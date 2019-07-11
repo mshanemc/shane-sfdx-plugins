d5bdc7eaf3a4e5d3cbb9c5c8215e8e094e524939
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-11 13:52:57 -0500

    put key in place accessible to app

--- remoteCI.sh
+++ remoteCI.sh
@@ -1,5 +1,7 @@
 echo "hello from a CI test-script"
 # Save a keyfile
-echo "$JWTKEY" > server.key
+mkdir -p app
+mkdir -p app/tmp
+echo "$JWTKEY" > app/tmp/server.key
 # Auth to SFDX
-sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile server.key --setdefaultdevhubusername -a hub
+sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile app/tmp/server.key --setdefaultdevhubusername -a hub
