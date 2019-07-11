10db71ff4c9f4662517c774775812fab0625d92d
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-11 14:08:05 -0500

    try profile instead of script.  Also, link plugin?

---
+++ .profile
@@ -0,0 +1,7 @@
+# Save a keyfile
+mkdir -p app/tmp
+echo "$JWTKEY" > app/tmp/server.key
+
+# Auth to SFDX
+sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile /app/tmp/server.key --setdefaultdevhubusername -a hub
+sfdx plugins:link .
--- remoteCI.sh
+++ remoteCI.sh
@@ -1,6 +1,1 @@
 echo "hello from a CI test-script"
-# Save a keyfile
-mkdir -p app/tmp
-echo "$JWTKEY" > app/tmp/server.key
-# Auth to SFDX
-sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile app/tmp/server.key --setdefaultdevhubusername -a hub
