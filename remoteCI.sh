2d0284dc6c65b593b4032b226beb832142fb5ba5
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-11 12:58:08 -0500

    remoteCI and +x

--- remoteCI.sh
+++ remoteCI.sh
@@ -1,1 +1,5 @@
 echo "hello from a CI test-script"
+# Save a keyfile
+# $JWTKEY > server.key
+# Auth to SFDX
+# sfdx force:auth:jwt:grant --cliendId $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile server.key --setdefaultdevhubusername -a hub
