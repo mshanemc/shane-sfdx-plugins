b195a8bd66085ad7869881d3e120d911aeea4c21
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-11 13:21:43 -0500

    try setting up auth stuff

--- remoteCI.sh
+++ remoteCI.sh
@@ -1,5 +1,6 @@
 echo "hello from a CI test-script"
 # Save a keyfile
-# $JWTKEY > server.key
+echo $JWTKEY
+$JWTKEY > server.key
 # Auth to SFDX
-# sfdx force:auth:jwt:grant --cliendId $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile server.key --setdefaultdevhubusername -a hub
+sfdx force:auth:jwt:grant --cliendId $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile server.key --setdefaultdevhubusername -a hub
