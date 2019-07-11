f914b15e0ff0501cd6458461185e30ab8ccf197e
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-11 13:35:54 -0500

    correct capitalization

--- remoteCI.sh
+++ remoteCI.sh
@@ -2,4 +2,4 @@ echo "hello from a CI test-script"
 # Save a keyfile
 echo "$JWTKEY" > server.key
 # Auth to SFDX
-sfdx force:auth:jwt:grant --clientId $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile server.key --setdefaultdevhubusername -a hub
+sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile server.key --setdefaultdevhubusername -a hub
