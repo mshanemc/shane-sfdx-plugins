af8b011b2c35f649930c3bb7818e087ff95fbe06
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-11 13:30:48 -0500

    echo to a file and fix grant command

--- remoteCI.sh
+++ remoteCI.sh
@@ -1,8 +1,5 @@
 echo "hello from a CI test-script"
 # Save a keyfile
-echo $JWTKEY
-echo $CONSUMERKEY
-echo $HUB_USERNAME
-$JWTKEY > server.key
+echo "$JWTKEY" > server.key
 # Auth to SFDX
-sfdx force:auth:jwt:grant --cliendId $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile server.key --setdefaultdevhubusername -a hub
+sfdx force:auth:jwt:grant --clientId $CONSUMERKEY --username $HUB_USERNAME --jwtkeyfile server.key --setdefaultdevhubusername -a hub
