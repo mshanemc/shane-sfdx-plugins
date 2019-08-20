d560768262cb4f21f6635e7f45e60f839b35472f
mshanemc <shane.mclaughlin@salesforce.com>
2019-08-20 09:40:45 -0500

    theme activation test

---
+++ test/commands/shane/theme/activate.test.ts
@@ -0,0 +1,27 @@
+import fs = require('fs-extra');
+
+import { exec, exec2JSON } from '../../../../src/shared/execProm';
+import testutils = require('../../../helpers/testutils');
+
+const testProjectName = 'testProjectThemeActivate';
+
+describe('shane:theme:activate', () => {
+    jest.setTimeout(testutils.remoteTimeout);
+
+    beforeEach(async () => {
+        await fs.remove(testProjectName);
+        await exec(`git clone https://github.com/mshanemc/electron-theme ${testProjectName}`);
+        await testutils.orgCreate(testProjectName);
+        await exec('sfdx force:source:push', { cwd: testProjectName });
+    });
+
+    it('activates the electron theme with --json', async () => {
+        const response = await exec2JSON(`sfdx shane:theme:activate -n Electron --json`, { cwd: testProjectName });
+        expect(response.status).toBe(0);
+    });
+
+    afterEach(async () => {
+        await testutils.orgDelete(testProjectName);
+        await fs.remove(testProjectName);
+    });
+});
