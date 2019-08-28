864065c83fb0f158fc2a61f5d6b4225e7b36ad63
mshanemc <shane.mclaughlin@salesforce.com>
2019-08-28 10:03:41 -0500

    better error messaging for src:install

--- src/commands/shane/github/src/install.ts
+++ src/commands/shane/github/src/install.ts
@@ -33,29 +33,43 @@ export default class GithubPackageInstall extends SfdxCommand {
         await fs.remove(this.flags.repo);
         const repoUrl = `https://github.com/${this.flags.githubuser}/${this.flags.repo}`;
 
-        const gitResult = await exec(`git clone ${repoUrl}`);
-        this.ux.log(gitResult.stderr);
+        this.ux.startSpinner(`cloning from ${repoUrl}`);
+        await exec(`git clone ${repoUrl}`);
+        this.ux.stopSpinner();
+
+        if (!fs.existsSync(`${this.flags.repo}/${this.flags.path}`)) {
+            if (!this.flags.keeplocally) {
+                await fs.remove(this.flags.repo);
+            }
+            throw new Error(`there is nothing at '${this.flags.path}'.  Check your --path flag`);
+        }
 
         if (this.flags.convert) {
+            this.ux.startSpinner(`converting source`);
             // convert to temp dir
             await exec(`sfdx force:source:convert -r ${this.flags.path} -d tmpSrcFolder`, { cwd: this.flags.repo });
             // delete the original source
             await fs.remove(`${this.flags.repo}/${this.flags.path}`);
             // put the converted source where the original was
             await fs.move(`${this.flags.repo}/tmpSrcFolder`, `${this.flags.repo}/${this.flags.path}`);
+            this.ux.stopSpinner();
         }
 
+        this.ux.startSpinner(`deploying to org`);
         // install in the org via mdapi
         const installResult = await exec2JSON(
             `sfdx force:mdapi:deploy -d ${this.flags.repo}/${this.flags.path} -u ${this.org.getUsername()} -w 20 --json`
         );
+        this.ux.stopSpinner();
 
         if (!this.flags.json) {
             this.ux.logJson(installResult);
         }
 
         if (!this.flags.keeplocally) {
+            this.ux.startSpinner(`cleaning up local source`);
             await fs.remove(this.flags.repo);
+            this.ux.stopSpinner();
         }
 
         return installResult;
