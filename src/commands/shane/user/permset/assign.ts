2c8bc01452dceda8554f5d3c7a4a0581511b2914
mshanemc <shane.mclaughlin@salesforce.com>
2020-01-27 09:55:03 -0600

    cleaner

--- src/commands/shane/user/permset/assign.ts
+++ src/commands/shane/user/permset/assign.ts
@@ -23,17 +23,12 @@ export default class UserPermsetAssign extends SfdxCommand {
 
     // tslint:disable-next-line:no-any
     public async run(): Promise<any> {
-        // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
         // tslint:disable-next-line: no-any
-        const conn: any = <unknown>this.org.getConnection();
-        let user;
-        if (this.flags.lastname) {
-            user = await userIdLookup.getUserId(conn, this.flags.lastname, this.flags.firstname);
-        } else {
-            // default to the user that you're connected as
-            const username = (await conn.identity()).username;
-            user = await singleRecordQuery({ conn, query: `select id from User where username ='${username}'` });
-        }
+        const conn = this.org.getConnection() as any;
+
+        const user = this.flags.lastname
+            ? await userIdLookup.getUserId(conn, this.flags.lastname, this.flags.firstname)
+            : await singleRecordQuery({ conn, query: `select id from User where username ='${(await conn.identity()).username}'` });
 
         this.ux.log(`found user with id ${user.Id}`);
 
@@ -46,19 +41,18 @@ export default class UserPermsetAssign extends SfdxCommand {
 
         this.ux.log(`found permset with id ${permset.Id}`);
 
-        let result;
-        try {
-            result = await conn.create('PermissionSetAssignment', {
+        const result = await conn
+            .create('PermissionSetAssignment', {
                 PermissionSetId: permset.Id,
                 AssigneeId: user.Id
+            })
+            .catch(e => {
+                if (e.name === 'DUPLICATE_VALUE') {
+                    // that's ok to swallow...it was already done, and this command is meant to be idempotent.  If the org is as you asked it to be, that's ok.
+                } else {
+                    throw new Error(e);
+                }
             });
-        } catch (result) {
-            if (result.name === 'DUPLICATE_VALUE') {
-                // that's ok...it was already done, and this command is meant to be idempotent.  If the org is as you asked it to be, that's ok.
-            } else {
-                throw new Error(result);
-            }
-        }
         this.ux.log(`User ${user.Id} has been assigned permset ${this.flags.name} (${permset.Id})`);
 
         return result;
