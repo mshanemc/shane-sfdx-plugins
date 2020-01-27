26700c1c2e5baa7acddf83987b02faa68bf2e321
mshanemc <shane.mclaughlin@salesforce.com>
2020-01-27 09:45:08 -0600

    cleaner, destructured requests

--- src/commands/shane/user/photo.ts
+++ src/commands/shane/user/photo.ts
@@ -27,43 +27,24 @@ export default class Photo extends SfdxCommand {
         banner: flags.filepath({ char: 'b', description: 'local path of the chatter banner photo to use', exclusive: ['file'] })
     };
 
-    // Comment this out if your command does not require an org username
     protected static requiresUsername = true;
-
-    // Comment this out if your command does not support a hub org username
-    // protected static supportsDevhubUsername = true;
-
-    // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
     protected static requiresProject = false;
 
     // tslint:disable-next-line:no-any
     public async run(): Promise<any> {
-        // const name = this.flags.name || 'world';
-
         // potential errors
         if (!this.flags.file && !this.flags.banner) {
             this.ux.error(chalk.red('you have to supply either --banner or --file'));
         }
 
-        // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
         const conn = this.org.getConnection();
-        // const query = 'Select Name, TrialExpirationDate from Organization';
-        let user;
-
-        try {
-            user = await userIdLookup.getUserId(conn, this.flags.lastname, this.flags.firstname);
-        } catch (e) {
-            this.ux.error(chalk.red(e));
-            throw new Error(e);
-        }
-
+        const user = await userIdLookup.getUserId(conn, this.flags.lastname, this.flags.firstname);
         this.ux.log(`found user with id ${user.Id}`);
-        const userid = user.Id;
 
         // still here?  you must be doing an attachment
         const options = {
             method: 'POST',
-            uri: `${conn.instanceUrl}/services/data/v42.0/connect/user-profiles/${userid}/photo`,
+            uri: `${conn.instanceUrl}/services/data/v42.0/connect/user-profiles/${user.Id}/photo`,
             json: true,
             body: {},
             headers: {
@@ -73,19 +54,23 @@ export default class Photo extends SfdxCommand {
 
         if (this.flags.file) {
             const photoCV = <Record>await localFile2CV.file2CV(conn, this.flags.file);
-            options.uri = `${conn.instanceUrl}/services/data/v42.0/connect/user-profiles/${userid}/photo`;
-            options.body = {
-                fileId: photoCV.ContentDocumentId
-            };
-            const photoResult = await request(options);
+            const photoResult = await request({
+                ...options,
+                uri: `${conn.instanceUrl}/services/data/v42.0/connect/user-profiles/${user.Id}/photo`,
+                body: {
+                    fileId: photoCV.ContentDocumentId
+                }
+            });
             return photoResult;
         } else if (this.flags.banner) {
             const bannerCV = <Record>await localFile2CV.file2CV(conn, this.flags.banner);
-            options.uri = `${conn.instanceUrl}/services/data/v42.0/connect/user-profiles/${userid}/banner-photo`;
-            options.body = {
-                fileId: bannerCV.ContentDocumentId
-            };
-            const bannerResult = await request(options);
+            const bannerResult = await request({
+                ...options,
+                uri: `${conn.instanceUrl}/services/data/v42.0/connect/user-profiles/${user.Id}/banner-photo`,
+                body: {
+                    fileId: bannerCV.ContentDocumentId
+                }
+            });
             return bannerResult;
         }
     }
