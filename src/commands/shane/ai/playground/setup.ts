7d408c6a28b1d0a3cd0d7d7fc7256cbf90db585a
mshanemc <shane.mclaughlin@salesforce.com>
2020-02-24 16:30:30 -0600

    playground from heroku app

--- src/commands/shane/ai/playground/setup.ts
+++ src/commands/shane/ai/playground/setup.ts
@@ -1,11 +1,8 @@
 import { flags, SfdxCommand } from '@salesforce/command';
-import { CreateResult, ContentVersion } from '../../../../shared/typeDefs';
-
-import localFile2CV = require('../../../../shared/localFile2CV');
+import { PlaygroundSetup } from '../../../../shared/aiPlaygroundSetup';
 
 const envVarKey = 'AI_PLAYGROUND_SETUP_KEY';
 const envVarEmail = 'EINSTEIN_EMAIL';
-const FILENAME = 'einstein_platform';
 
 export default class AIPlaygroundSetup extends SfdxCommand {
     public static description = 'upload .pem file from local encrypted copy, setup username and secret key in custom setting';
@@ -29,27 +26,24 @@ export default class AIPlaygroundSetup extends SfdxCommand {
         file: flags.filepath({ char: 'f', description: 'encrypted file from local filesystem', required: true }),
         email: flags.email({
             char: 'e',
-            description: 'email address you used when you signed up for your einstein.ai account',
-            default: process.env[envVarEmail]
+            description: `email address you used when you signed up for your einstein.ai account.  Defaults to ${envVarEmail} from the environment`,
+            env: process.env[envVarEmail]
         }),
-        key: flags.string({ char: 'k', description: `encoding key used to encrypt/decrypt the file.  Defaults to ${envVarKey} from the environment` })
+        key: flags.string({
+            char: 'k',
+            description: `encoding key used to encrypt/decrypt the file.  Defaults to ${envVarKey} from the environment`,
+            env: process.env[envVarKey]
+        })
     };
 
     protected static requiresUsername = true;
 
     public async run(): Promise<any> {
-        const conn = this.org.getConnection();
-        this.ux.startSpinner('uploading the pem file');
-        const CV = (await localFile2CV.file2CV(conn, this.flags.file, FILENAME)) as ContentVersion;
-        this.ux.setSpinnerStatus('modifying your custom setting');
-
-        const createResult = (await conn.sobject('einsteinplay__Einstein_Settings__c').create({
-            einsteinplay__Einstein_EMail__c: this.flags.email,
-            einsteinplay__CertName__c: CV.ContentDocumentId,
-            einsteinplay__Secret_Key__c: this.flags.key ?? process.env[envVarKey]
-        })) as CreateResult;
-
-        this.ux.stopSpinner(`created custom setting with id ${createResult.id}`);
-        return createResult.id;
+        return PlaygroundSetup({
+            conn: this.org.getConnection(),
+            filepath: this.flags.file,
+            email: this.flags.email,
+            key: this.flags.key ?? process.env[envVarKey]
+        });
     }
 }
---
+++ src/commands/shane/ai/playground/setupHeroku.ts
@@ -0,0 +1,123 @@
+import { flags, SfdxCommand } from '@salesforce/command';
+import { retry } from '@lifeomic/attempt';
+import * as fs from 'fs-extra';
+
+import { PlaygroundSetup } from '../../../../shared/aiPlaygroundSetup';
+
+import crypto = require('crypto');
+import aesjs = require('aes-js');
+import requestPromise = require('request-promise-native');
+
+const herokuAPIendpoint = 'https://api.heroku.com';
+
+const defaultHerokuRequest = {
+    headers: {
+        Authorization: `Bearer ${process.env.HEROKU_API_KEY}`,
+        Accept: 'application/vnd.heroku+json; version=3'
+    },
+    json: true
+};
+
+const tempFileName = 'einstein-key.pem';
+const cryptoBytes = 16;
+
+export default class AIPlaygroundSetupHeroku extends SfdxCommand {
+    public static description = 'provisions a new einstein.ai account and sets up the org';
+
+    public static examples = [
+        `sfdx shane:ai:playground:herokuSetup -a my-existing-app
+    // creates addons to existing app
+    `,
+        `sfdx shane:ai:playground:herokuSetup -c
+    // creates an app with whatever name heroku feels like
+    `,
+        `sfdx shane:ai:playground:herokuSetup -a non-existing-app -c
+    // creates a new app with the name of your choice (usually build dynamically!)
+    `
+    ];
+
+    protected static flagsConfig = {
+        app: flags.string({ char: 'a', description: 'name of the heroku app that we attach add-ons to' }),
+        create: flags.boolean({ char: 'c', description: 'create the app' }),
+        verbose: flags.builtin()
+    };
+
+    protected static requiresUsername = true;
+
+    public async run(): Promise<any> {
+        if (this.flags.create) {
+            this.ux.startSpinner('creating app on heroku');
+
+            // attach the einstein addon
+            const appResponse = await requestPromise.post(`${herokuAPIendpoint}/apps`, {
+                ...defaultHerokuRequest,
+                body: this.flags.app
+                    ? {
+                          name: this.flags.app
+                      }
+                    : {}
+            });
+            this.ux.stopSpinner(`created app ${appResponse.name}`);
+        }
+
+        this.ux.startSpinner('provisioning einstein addon');
+        // attach the einstein addon
+        const addonResponse = await requestPromise.post(`${herokuAPIendpoint}/apps/${this.flags.app}/addons`, {
+            ...defaultHerokuRequest,
+            body: {
+                plan: 'einstein-vision:starter',
+                config: { ACCOUNT_ID: 'm.shane.mclaughlin@gmail.com' }
+            }
+        });
+        // this.ux.logJson(addonResponse);
+        this.ux.setSpinnerStatus('getting ai credentials');
+        // get the config information
+        const addonConfig = await retry(
+            async () => {
+                const potentialResult = await requestPromise.get(`${herokuAPIendpoint}/apps/${this.flags.app}/config-vars`, {
+                    ...defaultHerokuRequest
+                });
+                if (potentialResult[addonResponse.config_vars.find(item => item.includes('ACCOUNT_ID'))].includes('heroku-')) {
+                    throw new Error('no valid email for accountId');
+                }
+                return potentialResult;
+            },
+            {
+                maxAttempts: 30,
+                async handleTimeout(result) {
+                    this.ux.logJson(result);
+                    throw new Error('no valid email for accountId');
+                }
+            }
+        );
+
+        // this.ux.logJson(addonConfig);
+        this.ux.setSpinnerStatus('creating security certificate locally');
+
+        const specificKey = addonConfig[addonResponse.config_vars.find(item => item.includes('PRIVATE_KEY'))].trim();
+        const specificEmail = addonConfig[addonResponse.config_vars.find(item => item.includes('ACCOUNT_ID'))];
+        const iv = crypto.randomBytes(cryptoBytes);
+        const encryptionKey = crypto.randomBytes(cryptoBytes);
+        const specificKeyAsBytes = aesjs.utils.utf8.toBytes(specificKey);
+
+        // eslint-disable-next-line new-cap
+        const aesCbc = new aesjs.ModeOfOperation.cbc(encryptionKey.toJSON().data, iv.toJSON().data);
+        const encryptedAsBytes = aesCbc.encrypt(aesjs.padding.pkcs7.pad(specificKeyAsBytes));
+
+        // write the file to a local path
+        await fs.writeFile(tempFileName, Buffer.concat([iv, encryptedAsBytes]), { encoding: 'binary' });
+        this.ux.setSpinnerStatus('connecting Salesfore org to einstein.ai');
+
+        // call the playgroundSetup stuff
+        const playgroundResult = await PlaygroundSetup({
+            conn: this.org.getConnection(),
+            filepath: tempFileName,
+            email: specificEmail,
+            key: encryptionKey.toString('base64')
+        });
+
+        await fs.remove(tempFileName); // delete the key file
+
+        return playgroundResult;
+    }
+}
