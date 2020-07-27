4b80536e6ff935ed7f0018dfd0be2244ec2ca088
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-27 09:30:53 -0500

    remove wait flag

--- src/shared/flags.ts
+++ src/shared/flags.ts
@@ -2,4 +2,4 @@ import { flags } from '@salesforce/command';
 
 const replay = flags.integer({ char: 'r', description: 'replay Id to begin from', default: -1 });
 
-export { replay, wait };
+export { replay };
